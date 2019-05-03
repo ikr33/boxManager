06a9ac9d737d9acd4df18bcc814a33ffcdbf66e8
Igor <ikr33@yahoo.com>
2019-05-02 19:54:17 -0700

    add env to read config.json path

--- doc_manager/settings.py
+++ doc_manager/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import json
 
-with open('/etc/config.json') as config_file:
+jsonPath = os.getenv('configjsonPath','')
+with open(jsonPath+'/etc/config.json') as config_file:
 	config = json.load(config_file)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
